5b5002e130eef1d730e9643f6525e649ec604365
tuantran0910 <tntuan0910@gmail.com>
2025-02-04 10:56:14 +0700

    fix: update seller link field to be non-nullable in model and migration

--- src/api/internal/models/sellerModel.go
+++ src/api/internal/models/sellerModel.go
@@ -6,6 +6,6 @@ type Seller struct {
 	BaseGormModel
 	UserID uuid.UUID `json:"user_id" gorm:"unique;not null"`
 	Name   string    `json:"name"`
-	Link   string    `json:"link" gorm:"unique;null"`
+	Link   string    `json:"link" gorm:"unique;not null"`
 	Logo   string    `json:"logo" gorm:"unique;null"`
 }
