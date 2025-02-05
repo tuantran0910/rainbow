a8d36c70497217a418f45bc837a1017525ae0875
tuantran0910 <tntuan0910@gmail.com>
2025-02-05 13:30:40 +0700

    feat: add Clickhouse configuration and initialization scripts

---
+++ deployments/docker/scripts/clickhouse/init.sql
@@ -0,0 +1,14 @@
+---------------------------------------------------------------------
+--                      DBT                                        --
+---------------------------------------------------------------------
+
+-- Initialize the necessary resources for the DBT
+CREATE DATABASE raw;
+CREATE DATABASE warehouse;
+
+---------------------------------------------------------------------
+--                      Cube.js Superset                           --
+---------------------------------------------------------------------
+
+-- Initialize the necessary resources for the Cube.js and Superset
+CREATE DATABASE analytics;
