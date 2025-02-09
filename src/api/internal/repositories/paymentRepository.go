225d05da9dddbabf98023d55e557448e5031ca27
tuantran0910 <tntuan0910@gmail.com>
2025-02-09 23:30:43 +0700

    refactor: remove unused total payments count from GetPayments method in payment repository

--- src/api/internal/repositories/paymentRepository.go
+++ src/api/internal/repositories/paymentRepository.go
@@ -23,11 +23,6 @@ func NewPaymentRepository(db *gorm.DB) IPaymentRepository {
 }
 
 func (pr *paymentRepository) GetPayments(ctx context.Context) ([]*models.Payment, error) {
-	var totalPayments int64
-	if err := pr.db.WithContext(ctx).Model(&models.Payment{}).Count(&totalPayments).Error; err != nil {
-		return nil, fmt.Errorf("failed to fetch total number of payments: %w", err)
-	}
-
 	var payments []*models.Payment
 	if err := pr.db.WithContext(ctx).Find(&payments).Error; err != nil {
 		return nil, fmt.Errorf("failed to fetch payments: %w", err)
