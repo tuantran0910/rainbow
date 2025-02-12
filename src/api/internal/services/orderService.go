75844b15e8f07e8ccdf9d1efd04d1bbefadf5af1
tuantran0910 <tntuan0910@gmail.com>
2025-02-13 00:27:36 +0700

    feat: add order controller and DTOs for order management with CRUD operations

---
+++ src/api/internal/services/orderService.go
@@ -0,0 +1,264 @@
+package services
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/tuantran0910/rainbow/internal/dtos"
+	"github.com/tuantran0910/rainbow/internal/models"
+	"github.com/tuantran0910/rainbow/internal/repositories"
+	"github.com/tuantran0910/rainbow/pkg/pagination"
+	"gorm.io/gorm"
+)
+
+type IOrderService interface {
+	GetOrdersByUserId(
+		ctx context.Context,
+		page, limit int,
+		currentUserId uuid.UUID,
+	) ([]*models.Order, *pagination.Pagination, error)
+	GetOrderById(
+		ctx context.Context,
+		orderId uuid.UUID,
+		currentUserId uuid.UUID,
+	) (*models.Order, error)
+	CreateOrder(
+		ctx context.Context,
+		orderRequest dtos.CreateOrderRequest,
+		currentUserId uuid.UUID,
+	) error
+	DeleteOrder(ctx context.Context, orderId uuid.UUID, currentUserId uuid.UUID) error
+}
+
+type orderService struct {
+	db                  *gorm.DB
+	orderRepository     repositories.IOrderRepository
+	bookRepository      repositories.IBookRepository
+	inventoryRepository repositories.IInventoryRepository
+	promotionRepository repositories.IPromotionRepository
+	userRepository      repositories.IUserRepository
+}
+
+func NewOrderService(
+	db *gorm.DB,
+	orderRepository repositories.IOrderRepository,
+	bookRepository repositories.IBookRepository,
+	inventoryRepository repositories.IInventoryRepository,
+	promoRepository repositories.IPromotionRepository,
+	userRepository repositories.IUserRepository,
+) IOrderService {
+	return &orderService{
+		db:                  db,
+		orderRepository:     orderRepository,
+		bookRepository:      bookRepository,
+		inventoryRepository: inventoryRepository,
+		promotionRepository: promoRepository,
+		userRepository:      userRepository,
+	}
+}
+
+func (os *orderService) withTX(
+	ctx context.Context,
+	fn func(
+		context.Context,
+		repositories.IOrderRepository,
+		repositories.IBookRepository,
+		repositories.IInventoryRepository,
+		repositories.IPromotionRepository,
+		repositories.IUserRepository,
+	) error,
+) error {
+	return os.db.WithContext(ctx).Transaction(func(tx *gorm.DB) error {
+		orderRepository := os.orderRepository.WithTX(tx)
+		bookRepository := os.bookRepository.WithTX(tx)
+		inventoryRepository := os.inventoryRepository.WithTX(tx)
+		promotionRepository := os.promotionRepository.WithTX(tx)
+		userRepository := os.userRepository.WithTX(tx)
+		return fn(
+			ctx,
+			orderRepository,
+			bookRepository,
+			inventoryRepository,
+			promotionRepository,
+			userRepository,
+		)
+	})
+}
+
+func (os *orderService) GetOrdersByUserId(
+	ctx context.Context,
+	page, limit int,
+	currentUserId uuid.UUID,
+) ([]*models.Order, *pagination.Pagination, error) {
+	return os.orderRepository.GetOrdersByUserId(ctx, page, limit, currentUserId)
+}
+
+func (os *orderService) GetOrderById(
+	ctx context.Context,
+	orderId uuid.UUID,
+	currentUserId uuid.UUID,
+) (*models.Order, error) {
+	return os.orderRepository.GetOrderById(ctx, orderId, currentUserId)
+}
+
+func (os *orderService) CreateOrder(
+	ctx context.Context,
+	orderRequest dtos.CreateOrderRequest,
+	currentUserId uuid.UUID,
+) error {
+	return os.withTX(ctx, func(
+		ctx context.Context,
+		orderRepository repositories.IOrderRepository,
+		bookRepository repositories.IBookRepository,
+		inventoryRepository repositories.IInventoryRepository,
+		promotionRepository repositories.IPromotionRepository,
+		userRepository repositories.IUserRepository,
+	) error {
+		var err error
+
+		order := &models.Order{
+			UserID:          currentUserId,
+			PaymentID:       orderRequest.PaymentId,
+			PromotionID:     orderRequest.PromotionId,
+			ShippingAddress: orderRequest.ShippingAddress,
+			TotalAmount:     0,
+		}
+		if err = orderRepository.CreateOrder(ctx, order); err != nil {
+			return err
+		}
+
+		bookTotalAmount := 0.0
+		for _, orderItem := range orderRequest.OrderItems {
+			book, err := bookRepository.GetBookById(ctx, orderItem.BookId)
+			if err != nil {
+				return err
+			}
+			if book == nil {
+				return fmt.Errorf("book not found")
+			}
+
+			if book.Inventory.Stock < orderItem.Quantity {
+				return fmt.Errorf("not enough stock for book with ID %s", book.ID)
+			}
+
+			bookTotalAmount += book.Price * float64(orderItem.Quantity)
+			orderItemModel := &models.OrderItem{
+				OrderID:   order.ID,
+				BookID:    orderItem.BookId,
+				Quantity:  orderItem.Quantity,
+				UnitPrice: book.Price,
+				Discount:  book.OriginalPrice - book.Price,
+			}
+			if err = orderRepository.CreateOrderItem(ctx, orderItemModel); err != nil {
+				return err
+			}
+
+			bookInventory := book.Inventory
+			bookInventory.Stock -= orderItem.Quantity
+			if err = inventoryRepository.UpdateInventory(ctx, book.Inventory.ID, &bookInventory); err != nil {
+				return err
+			}
+
+			book.SoldCount += orderItem.Quantity
+			if err = bookRepository.UpdateBook(ctx, book.ID, book); err != nil {
+				return err
+			}
+		}
+
+		var promotion *models.Promotion
+		if orderRequest.PromotionId != nil {
+			promotion, err = promotionRepository.GetPromotionById(ctx, *orderRequest.PromotionId)
+			if err != nil {
+				return err
+			}
+		}
+
+		if promotion != nil && promotion.UsedCount < promotion.MaxUses {
+			if promotion.StartDate.After(order.CreatedAt) ||
+				promotion.EndDate.Before(order.CreatedAt) {
+				return fmt.Errorf("promotion is not available")
+			}
+
+			*order.PromotionID = promotion.ID
+			if promotion.DiscountType == models.DiscountPercentage {
+				order.TotalAmount = bookTotalAmount * (1 - promotion.DiscountValue/100)
+			} else {
+				order.TotalAmount = bookTotalAmount - promotion.DiscountValue
+			}
+
+			if order.TotalAmount < 0 {
+				order.TotalAmount = 0
+			}
+
+			promotion.UsedCount++
+			if err = promotionRepository.UpdatePromotion(ctx, promotion.ID, promotion); err != nil {
+				return err
+			}
+		}
+
+		if err = orderRepository.UpdateOrder(ctx, order); err != nil {
+			return err
+		}
+		return nil
+	})
+}
+
+func (os *orderService) DeleteOrder(
+	ctx context.Context,
+	orderId uuid.UUID,
+	currentUserId uuid.UUID,
+) error {
+	return os.withTX(ctx, func(
+		ctx context.Context,
+		orderRepository repositories.IOrderRepository,
+		bookRepository repositories.IBookRepository,
+		inventoryRepository repositories.IInventoryRepository,
+		promotionRepository repositories.IPromotionRepository,
+		userRepository repositories.IUserRepository,
+	) error {
+		order, err := orderRepository.GetOrderById(ctx, orderId, currentUserId)
+		if err != nil {
+			return err
+		}
+		if order == nil {
+			return fmt.Errorf("order not found")
+		}
+
+		for _, orderItem := range order.OrderItems {
+			book, err := bookRepository.GetBookById(ctx, orderItem.BookID)
+			if err != nil {
+				return err
+			}
+			if book == nil {
+				return fmt.Errorf("book not found")
+			}
+
+			book.Inventory.Stock += orderItem.Quantity
+			if err := inventoryRepository.UpdateInventory(ctx, book.Inventory.ID, &book.Inventory); err != nil {
+				return err
+			}
+
+			book.SoldCount -= orderItem.Quantity
+			if err := bookRepository.UpdateBook(ctx, book.ID, book); err != nil {
+				return err
+			}
+		}
+
+		if order.PromotionID != nil {
+			promotion, err := promotionRepository.GetPromotionById(ctx, *order.PromotionID)
+			if err != nil {
+				return err
+			}
+			if promotion != nil && promotion.StartDate.Before(order.CreatedAt) &&
+				promotion.EndDate.After(order.CreatedAt) {
+				promotion.UsedCount--
+				if err := promotionRepository.UpdatePromotion(ctx, promotion.ID, promotion); err != nil {
+					return err
+				}
+			}
+		}
+
+		return orderRepository.DeleteOrder(ctx, orderId)
+	})
+}
