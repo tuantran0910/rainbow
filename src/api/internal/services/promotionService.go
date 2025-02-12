a06e5539cda7c7a93f954901ba254f6fc386efd9
tuantran0910 <tntuan0910@gmail.com>
2025-02-13 00:29:09 +0700

    feat: refactor promotion service and repository for improved promotion management

--- src/api/internal/services/promotionService.go
+++ src/api/internal/services/promotionService.go
@@ -12,7 +12,11 @@ import (
 )
 
 type IPromotionService interface {
-	CreatePromotion(ctx context.Context, promotionRequest dtos.CreatePromotionRequest, currentUserId uuid.UUID) error
+	CreatePromotion(
+		ctx context.Context,
+		promotionRequest dtos.CreatePromotionRequest,
+		currentUserId uuid.UUID,
+	) error
 }
 
 type promotionService struct {
@@ -21,7 +25,11 @@ type promotionService struct {
 	userRepository      repositories.IUserRepository
 }
 
-func NewPromotionService(db *gorm.DB, promotionRepository repositories.IPromotionRepository, userRepository repositories.IUserRepository) IPromotionService {
+func NewPromotionService(
+	db *gorm.DB,
+	promotionRepository repositories.IPromotionRepository,
+	userRepository repositories.IUserRepository,
+) IPromotionService {
 	return &promotionService{
 		db:                  db,
 		promotionRepository: promotionRepository,
@@ -29,7 +37,10 @@ func NewPromotionService(db *gorm.DB, promotionRepository repositories.IPromotio
 	}
 }
 
-func (ps *promotionService) withTx(ctx context.Context, fn func(context.Context, repositories.IPromotionRepository, repositories.IUserRepository) error) error {
+func (ps *promotionService) withTX(
+	ctx context.Context,
+	fn func(context.Context, repositories.IPromotionRepository, repositories.IUserRepository) error,
+) error {
 	return ps.db.Transaction(func(tx *gorm.DB) error {
 		promotionRepository := ps.promotionRepository.WithTX(tx)
 		userRepository := ps.userRepository.WithTX(tx)
@@ -37,41 +48,35 @@ func (ps *promotionService) withTx(ctx context.Context, fn func(context.Context,
 	})
 }
 
-func (pc *promotionService) CreatePromotion(ctx context.Context, promotionRequest dtos.CreatePromotionRequest, currentUserId uuid.UUID) error {
-	return pc.withTx(ctx, func(ctx context.Context, promotionRepository repositories.IPromotionRepository, userRepository repositories.IUserRepository) error {
-		currentUser, err := userRepository.GetUserById(ctx, currentUserId)
-		if err != nil {
-			return err
-		}
-		if currentUser == nil {
-			return fmt.Errorf("current user not found")
-		}
-
-		if currentUser.Role != models.AdminRole {
-			return fmt.Errorf("current user does not have permission to create promotion")
-		}
-
-		promotion := &models.Promotion{
-			Name:          promotionRequest.Name,
-			DiscountType:  promotionRequest.DiscountType,
-			DiscountValue: promotionRequest.DiscountValue,
-			StartDate:     promotionRequest.StartDate,
-			EndDate:       promotionRequest.EndDate,
-			MaxUses:       promotionRequest.MaxUses,
-		}
-		if err := promotionRepository.CreatePromotion(ctx, promotion); err != nil {
-			return err
-		}
-
-		for _, bookId := range promotionRequest.BookIds {
-			promotionBook := &models.PromotionBook{
-				PromotionID: promotion.ID,
-				BookID:      bookId,
-			}
-			if err := promotionRepository.AddBookToPromotion(ctx, promotionBook); err != nil {
+func (pc *promotionService) CreatePromotion(
+	ctx context.Context,
+	promotionRequest dtos.CreatePromotionRequest,
+	currentUserId uuid.UUID,
+) error {
+	return pc.withTX(
+		ctx,
+		func(ctx context.Context, promotionRepository repositories.IPromotionRepository, userRepository repositories.IUserRepository) error {
+			currentUser, err := userRepository.GetUserById(ctx, currentUserId)
+			if err != nil {
 				return err
 			}
-		}
-		return nil
-	})
+			if currentUser == nil {
+				return fmt.Errorf("current user not found")
+			}
+
+			if currentUser.Role != models.AdminRole {
+				return fmt.Errorf("current user does not have permission to create promotion")
+			}
+
+			promotion := &models.Promotion{
+				Name:          promotionRequest.Name,
+				DiscountType:  promotionRequest.DiscountType,
+				DiscountValue: promotionRequest.DiscountValue,
+				StartDate:     promotionRequest.StartDate,
+				EndDate:       promotionRequest.EndDate,
+				MaxUses:       promotionRequest.MaxUses,
+			}
+			return promotionRepository.CreatePromotion(ctx, promotion)
+		},
+	)
 }
