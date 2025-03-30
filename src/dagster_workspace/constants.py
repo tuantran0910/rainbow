91592275317987cdceae7b2cd7f5b0caf0472308
tuantran0910 <tntuan0910@gmail.com>
2025-03-30 21:13:21 +0700

    feat: integrate dbt support into Dagster workspace with custom asset translation and scheduling

--- src/dagster_workspace/constants.py
+++ src/dagster_workspace/constants.py
@@ -2,3 +2,5 @@ import os
 from pathlib import Path
 
 DAGSTER_ASSETS_CONFIG_DIR = Path(os.getenv("DAGSTER_ASSETS_CONFIG_DIR", "/opt/dagster/app/configs"))
+DAGSTER_DBT_TARGET_PROFILE = os.getenv("DAGSTER_DBT_TARGET_PROFILE", "production")
+DAGSTER_ASSETS_OWNER = ["tntuan0910@gmail.com"]
---
+++ src/dagster_workspace/dbt_project.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import dagster as dg
+from dagster_dbt import build_schedule_from_dbt_selection
+from dagster_dbt import dbt_assets
+from dagster_dbt import DbtCliResource
+from dagster_dbt import DbtProject
+
+from assets.translators import CustomDagsterDbtTranslator
+from constants import DAGSTER_DBT_TARGET_PROFILE
+
+# Points to the dbt project path
+dbt_project_dir = Path(__file__).absolute().parent.parent / "dbt"
+dbt_project = DbtProject(project_dir=dbt_project_dir, target=DAGSTER_DBT_TARGET_PROFILE)
+
+# References the dbt project object
+dbt_resource = DbtCliResource(project_dir=dbt_project)
+
+# Compiles the dbt project & allow Dagster to build an asset graph
+dbt_project.prepare_if_dev()
+
+
+def build_dbt_project() -> dg.Definitions:
+    """
+    Build the dbt project and return the Dagster definitions.
+
+    Returns:
+        dg.Definitions: Dagster definitions containing the configured assets.
+    """
+
+    # Create a dbt asset using the dbt project
+    @dbt_assets(
+        manifest=dbt_project.manifest_path,
+        dagster_dbt_translator=CustomDagsterDbtTranslator(),
+    )
+    def dbt_models(context: dg.AssetExecutionContext, dbt: DbtCliResource):
+        """
+        This function is used to compile the dbt project and allow Dagster to build an asset graph.
+        """
+        yield from (
+            dbt.cli(["build"], context=context).stream().fetch_row_counts().fetch_column_metadata()
+        )
+
+    # Create a schedule for the dbt models
+    dbt_schedule = build_schedule_from_dbt_selection(
+        [dbt_models],
+        job_name="dbt_materialization_job",
+        cron_schedule="0 0 * * *",
+        dbt_select="fqn:*",
+    )
+
+    return dg.Definitions(
+        assets=[dbt_models],
+        resources={"dbt": dbt_resource},
+        schedules=[dbt_schedule],
+    )
