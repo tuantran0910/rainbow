d6c2ba5c0cbe943a44e14732365996b3d5e79927
tuantran0910 <tntuan0910@gmail.com>
2025-04-13 23:34:28 +0700

    docs: enhance PostgresResource documentation with detailed argument and return descriptions

--- src/dagster_workspace/resources/psql_resource.py
+++ src/dagster_workspace/resources/psql_resource.py
@@ -5,6 +5,13 @@ import psycopg2
 class PostgresResource(dg.ConfigurableResource):
     """
     A resource for interacting with a PostgreSQL database within Dagster.
+
+    Args:
+        host (str): The host of the PostgreSQL database.
+        port (int): The port of the PostgreSQL database.
+        database (str): The name of the PostgreSQL database.
+        user (str): The user of the PostgreSQL database.
+        password (str): The password of the PostgreSQL database.
     """
 
     host: str
@@ -16,6 +23,9 @@ class PostgresResource(dg.ConfigurableResource):
     def get_conn(self) -> psycopg2.connect:
         """
         Get a connection to the PostgreSQL database.
+
+        Returns:
+            psycopg2.connect: A connection to the PostgreSQL database.
         """
         return psycopg2.connect(
             host=self.host,
@@ -25,9 +35,16 @@ class PostgresResource(dg.ConfigurableResource):
             password=self.password,
         )
 
-    def fetchall(self, query: str, params: tuple = ()):
+    def fetchall(self, query: str, params: tuple = ()) -> list[tuple]:
         """
         Fetch all rows from a query on the PostgreSQL database.
+
+        Args:
+            query (str): The query to execute.
+            params (tuple): The parameters to pass to the query.
+
+        Returns:
+            list[tuple]: A list of tuples containing the rows from the query.
         """
         with self.get_conn() as conn:
             with conn.cursor() as cursor:
