619614cef40eb20e9166ad70ca7ee5f2d3acc77c
tuantran0910 <tntuan0910@gmail.com>
2025-04-20 17:27:50 +0700

    feat: implement staging model macro for consistent data transformation across tables

--- src/dbt/models/staging/stg_payments.sql
+++ src/dbt/models/staging/stg_payments.sql
@@ -4,8 +4,4 @@
     )
 }}
 
-SELECT *
-EXCEPT(_dlt_load_id, _dlt_id),
-    toDate(created_at, 'Asia/Ho_Chi_Minh') AS created_at_tz_hcm,
-    toDate(updated_at, 'Asia/Ho_Chi_Minh') AS updated_at_tz_hcm
-FROM {{ source('rainbow', 'payments') }}
+{{ stg_model_sql(source('rainbow', 'payments')) }}
