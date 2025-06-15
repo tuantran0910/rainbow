4d314b4074968cb93b4676ef72455ac2524c3170
tuantran0910 <tntuan0910@gmail.com>
2025-06-16 02:07:46 +0700

    feat: add global address and DNS record for Superset ingress in terraform configuration

--- deployments/cloud/terraform/dns.tf
+++ deployments/cloud/terraform/dns.tf
@@ -3,6 +3,11 @@ resource "google_compute_global_address" "dagster_ingress_ip" {
   address_type = "EXTERNAL"
 }
 
+resource "google_compute_global_address" "superset_ingress_ip" {
+  name         = "superset-ingress-ip"
+  address_type = "EXTERNAL"
+}
+
 # Managed zone for tuantrann.work
 data "google_dns_managed_zone" "tuantrann_work" {
   name = "tuantrann-work-zone"
@@ -17,9 +22,18 @@ resource "google_dns_record_set" "dagster_dns" {
   rrdatas      = [google_compute_global_address.dagster_ingress_ip.address]
 }
 
+# DNS record for Superset ingress
+resource "google_dns_record_set" "superset_dns" {
+  name         = "superset.tuantrann.work."
+  managed_zone = data.google_dns_managed_zone.tuantrann_work.name
+  type         = "A"
+  ttl          = 300
+  rrdatas      = [google_compute_global_address.superset_ingress_ip.address]
+}
+
 # SSL Policy
 resource "google_compute_ssl_policy" "rainbow_ssl_policy" {
-  name = "rainbow-ssl-policy"
-  profile = "MODERN"
+  name            = "rainbow-ssl-policy"
+  profile         = "MODERN"
   min_tls_version = "TLS_1_2"
 }
