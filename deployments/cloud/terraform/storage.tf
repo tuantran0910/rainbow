68fb624d2444121fa9e4cff05d8eb118e4b7b389
tuantran0910 <tntuan0910@gmail.com>
2025-06-25 16:47:52 +0700

    chore: update kustomization and configuration files for CubeJS and remove commented resources

--- deployments/cloud/terraform/storage.tf
+++ deployments/cloud/terraform/storage.tf
@@ -1,8 +1,12 @@
-resource "google_storage_bucket" "cubejs" {
-  name                        = "rainbow-data-production-cubejs"
-  location                    = local.region
-  uniform_bucket_level_access = true
-  public_access_prevention    = "enforced"
+# resource "google_storage_bucket" "cubejs" {
+#   name                        = "rainbow-data-production-cubejs"
+#   location                    = local.region
+#   uniform_bucket_level_access = true
+#   public_access_prevention    = "enforced"
+# }
+
+data "google_storage_bucket" "cubejs" {
+  name = "${local.project_id}-cubejs"
 }
 
 # resource "google_storage_bucket" "dbt" {
