49a3a54319e8ee4aa43769378f0d2a9b74c2c231
tuantran0910 <tntuan0910@gmail.com>
2025-07-12 22:46:07 +0700

    chore: update maximum configuration values for books, items, and quantity in constants.py

--- src/dagster_workspace/dbt/cloud/models/intermediate/batch_layer/fct_sales__batch.sql
+++ src/dagster_workspace/dbt/cloud/models/intermediate/batch_layer/fct_sales__batch.sql
@@ -25,11 +25,11 @@
         orders_relation=ref('raw_orders'),
         order_items_relation=ref('raw_order_items'),
         dim_dates_relation=ref('dim_dates'),
-        dim_users_relation=ref('dim_users__batch'),
-        dim_books_relation=ref('dim_books__batch'),
-        dim_sellers_relation=ref('dim_sellers__batch'),
-        dim_promotions_relation=ref('dim_promotions__batch'),
-        dim_payments_relation=ref('dim_payments__batch'),
-        dim_categories_relation=ref('dim_categories__batch')
+        dim_users_relation=ref('dim_users'),
+        dim_books_relation=ref('dim_books'),
+        dim_sellers_relation=ref('dim_sellers'),
+        dim_promotions_relation=ref('dim_promotions'),
+        dim_payments_relation=ref('dim_payments'),
+        dim_categories_relation=ref('dim_categories')
     )
 }}
--- src/dagster_workspace/dbt/cloud/models/intermediate/speed_layer/fct_sales__stream.sql
+++ src/dagster_workspace/dbt/cloud/models/intermediate/speed_layer/fct_sales__stream.sql
@@ -12,12 +12,12 @@
         orders_relation=ref('raw_orders__datastream'),
         order_items_relation=ref('raw_order_items__datastream'),
         dim_dates_relation=ref('dim_dates'),
-        dim_users_relation=ref('dim_users__stream'),
-        dim_books_relation=ref('dim_books__stream'),
-        dim_sellers_relation=ref('dim_sellers__stream'),
-        dim_promotions_relation=ref('dim_promotions__stream'),
-        dim_payments_relation=ref('dim_payments__stream'),
-        dim_categories_relation=ref('dim_categories__stream'),
+        dim_users_relation=ref('dim_users'),
+        dim_books_relation=ref('dim_books'),
+        dim_sellers_relation=ref('dim_sellers'),
+        dim_promotions_relation=ref('dim_promotions'),
+        dim_payments_relation=ref('dim_payments'),
+        dim_categories_relation=ref('dim_categories'),
         is_stream=true
     )
 }}
