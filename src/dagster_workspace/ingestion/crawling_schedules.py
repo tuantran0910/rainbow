0fb192966078b1439f2bef9482ec3c08e835b313
tuantran0910 <tntuan0910@gmail.com>
2025-07-12 22:22:47 +0700

    chore: update cron schedule for tiki_crawling_schedule from '0 0 * * *' to '0 1 * * *'

--- src/dagster_workspace/ingestion/crawling_schedules.py
+++ src/dagster_workspace/ingestion/crawling_schedules.py
@@ -7,7 +7,7 @@ from shared.constants import DAGSTER_TAGS
 crawling_schedule = dg.ScheduleDefinition(
     name="tiki_crawling_schedule",
     description="This schedule is responsible for crawling tiki resources",
-    cron_schedule="0 0 * * *",
+    cron_schedule="0 1 * * *",
     job=crawling_job,
     metadata=DAGSTER_METADATA,
     tags=DAGSTER_TAGS,
